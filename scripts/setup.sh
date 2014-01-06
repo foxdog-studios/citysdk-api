d70f47ef2cba997e4f688c518858c9b6fa94f21a
Peter Sutton <foxxy@foxdogstudios.com>
2014-01-06 12:52:06 +0000

    Improved development setup for Arch Linux

---
+++ scripts/setup.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+
+# ==============================================================================
+# = Configuration                                                              =
+# ==============================================================================
+
+applications=(
+    server
+    cms
+    rdf
+    devsite
+)
+
+config_applications=(
+    server
+    cms
+    devsite
+)
+
+packages=(
+    git
+    libyaml
+    postgresql-libs
+)
+
+repo=$(realpath "$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")/..")
+
+ruby_version=1.9.3
+
+
+# ==============================================================================
+# = Helpers                                                                    =
+# ==============================================================================
+
+function ensure_line()
+{
+    local line=$1
+    local path=$2
+
+    if ! grep --fixed-strings  \
+              --line-regexp    \
+              --quiet          \
+              "--regexp=$line" \
+              "$path"; then
+        printf '%s\n' "$line" >> "$path"
+    fi
+}
+
+
+function rvmdo()
+{
+    # Leave 'do' quoted. Without, VIM gets confused.
+    ~/.rvm/bin/rvm "$ruby_version" 'do' "$@"
+}
+
+
+# ==============================================================================
+# = Tasks                                                                      =
+# ==============================================================================
+
+function packages-install()
+{
+    sudo pacman --noconfirm --sync --needed --refresh "${packages[@]}"
+}
+
+
+function ruby-rvm()
+{
+    # /etc/gemrc is part of Arch Linux's Ruby package
+    if [[ -f /etc/gemrc ]]; then
+        sudo sed -i '/gem: --user-install/d' /etc/gemrc
+    fi
+
+    curl --location https://get.rvm.io          \
+        | bash -s stable "--ruby=$ruby_version"
+
+    local path=~/.bash_profile
+    if [[ -f "$path" ]]; then
+        ensure_line 'source ~/.profile' "$path"
+    fi
+}
+
+
+function ruby-gems()
+{
+    local app
+
+    for app in "${applications[@]}"; do
+        echo Bundling: $app
+        rvmdo bundle install "--gemfile=$repo/$app/Gemfile"
+    done
+}
+
+
+function config-init()
+{
+    local config=$repo/config
+    local template=$config/config.template.sh
+    local config_local=$config/local
+
+    mkdir --parent "$config_local"
+
+    local development=$config_local/development.sh
+
+    if [[ ! -f "$development" ]]; then
+        cp "$template" "$development"
+    else
+        echo $development already exists, skipping
+    fi
+
+    local production=$config_local/production.sh
+
+    if [[ ! -f "$production" ]]; then
+        cp "$template" "$production"
+    else
+        echo $production already exists, skipping
+    fi
+}
+
+
+function config-ln()
+{
+    local app
+
+    for app in "${config_applications[@]}"; do
+        echo Soft-linking config for: $app
+        ln -f -s ../config/local/development.json "$repo/$app/config.json"
+    done
+}
+
+
+function manual()
+{
+    cat <<-'EOF'
+		1) Ensure $HOME/.rvm/bin is on your $PATH
+	EOF
+}
+
+# ==============================================================================
+# = Command line interface                                                     =
+# ==============================================================================
+
+all_tasks=(
+    packages-install
+    ruby-rvm
+    ruby-gems
+    config-init
+    config-ln
+    manual
+)
+
+function usage()
+{
+    cat <<-'EOF'
+		Set up a development environment
+
+		Usage:
+
+		    setup.sh TASK
+
+		Tasks:
+
+		    packages-install
+		    ruby-rvm
+		    ruby-gems
+		    config-init
+		    config-ln
+		    manual
+	EOF
+    exit 1
+}
+
+
+for task in "$@"; do
+    if [[ "$(type -t "$task" 2> /dev/null)" != function ]]; then
+        usage
+    fi
+done
+
+for task in "${@:-${all_tasks[@]}}"; do
+    echo -e "\e[5;32mTask: $task\e[0m"
+    "$task"
+done
+
