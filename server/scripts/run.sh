fa078e09cb751173a51e1a8c0e6fb74e4a341d5e
Peter Sutton <foxxy@foxdogstudios.com>
2014-01-07 11:02:35 +0000

    Fixed server config.ru

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 
 cd "$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")/.."
-exec rvm 1.9.3@citysdk do bundle exec rackup config.ru
+exec rvm 1.9.3@citysdk do bundle exec rackup
 
