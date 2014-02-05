e9d8ff00dd14da5b0aea8129e0edf6d3c876a629
Peter Sutton <foxxy@foxdogstudios.com>
2014-02-05 21:01:01 +0000

    Added some logging

--- server/create_admin.rb
+++ server/create_admin.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby -w
+#!/usr/bin/env ruby
 
 require 'json'
 require 'docopt'
@@ -16,13 +16,10 @@ DOCOPT = <<DOCOPT
 
 DOCOPT
 
-
 ARGS = Docopt::docopt(DOCOPT)
-
 
 SERVER_CONFIG_PATH = ARGS.fetch('SERVER_CONFIG')
 SETUP_CONFIG_PATH = ARGS.fetch('SETUP_CONFIG')
-
 
 def load_config(config_path)
   open(config_path) do |config_file|
@@ -30,36 +27,32 @@ def load_config(config_path)
   end # do
 end # def
 
-
 SERVER_CONFIG = load_config(SERVER_CONFIG_PATH)
 SETUP_CONFIG = load_config(SETUP_CONFIG_PATH)
 ADMIN_CONFIG = SETUP_CONFIG.fetch('admin')
-
 
 def get_admin(key)
   ADMIN_CONFIG.fetch(key.to_s)
 end # def
 
-
 ADMIN_EMAIL = get_admin(:email)
 ADMIN_PASSWORD = get_admin(:password)
 ADMIN_DOMAINS = get_admin(:domains).split(',')
 
-
 def get_db(key)
   SERVER_CONFIG.fetch("db_#{ key.to_s }")
 end # def
-
 
 DB_NAME = get_db(:name)
 DB_HOST = get_db(:host)
 DB_USER = get_db(:user)
 DB_PASSWORD = get_db(:pass)
 
-
 # ==============================================================================
 # = Database connection                                                        =
 # ==============================================================================
+
+print 'Connecting to database...'
 
 require 'sequel'
 
@@ -72,10 +65,14 @@ DB = Sequel.postgres(
 
 DB.extension(:pg_array)
 
+puts 'done'
+
 
 # ==============================================================================
 # = Create admin user                                                          =
 # ==============================================================================
+
+print 'Creating or updating admin user...'
 
 require 'sinatra-authentication'
 
@@ -83,7 +80,11 @@ USER = User.set(
   email:                 ADMIN_EMAIL,
   password:              ADMIN_PASSWORD,
   password_confirmation: ADMIN_PASSWORD,
+  # Admin permission level
   permission_level:      -1,
   domains:               Sequel.pg_array(ADMIN_DOMAINS)
 )
+
+puts 'done'
+puts 'All done, goodbye!'
 
