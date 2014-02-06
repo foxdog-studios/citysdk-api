996ae7c1a7e53a1198d5fe194f83cb8322449f82
Peter Sutton <foxxy@foxdogstudios.com>
2014-02-06 19:30:11 +0000

    Removed nesting

--- server/hooks/before/basic_authentication.rb
+++ server/hooks/before/basic_authentication.rb
@@ -8,12 +8,11 @@ class CitySDKAPI < Sinatra::Application
   before do
     # Basic authentication
     header_name = 'HTTP_AUTHORIZATION'
-    if request.env.key?(header_name)
-      header_value = request.env.fetch(header_name)
-      email, password = HTTPAuth::Basic.unpack_authorization(header_value)
-      user = User.authenticate(email, password)
-      session[:user] = user.id unless user.nil?
-    end # if
+    return unless request.env.key?(header_name)
+    header_value = request.env.fetch(header_name)
+    email, password = HTTPAuth::Basic.unpack_authorization(header_value)
+    user = User.authenticate(email, password)
+    session[:user] = user.id unless user.nil?
   end # do
 
 end # class
