4df25d7b532715440a414af09366ef50eee93084
Peter Sutton <foxxy@foxdogstudios.com>
2014-04-04 16:28:15 +0100

    Moved node datum serialization

--- server/utils/serializers/serializer.rb
+++ server/utils/serializers/serializer.rb
@@ -32,6 +32,41 @@ module CitySDK
         serialize_data_datum(node, node_datum, field, params)
       end # unless
     end # def
+
+    def serialize_node_datum(cdk_id, h, params)
+      newh = {}
+      h.each do |nd|
+        layer_id = nd[:layer_id]
+        name = Layer.nameFromId(layer_id)
+
+        nd.delete(:validity)
+        nd.delete(:tags) if nd[:tags].nil?
+
+        if nd[:modalities]
+          nd[:modalities] = nd[:modalities].map { |m| Modality.name_for_id(m) }
+        else
+          nd.delete(:modalities)
+        end
+
+        nd.delete(:id)
+        nd.delete(:node_id)
+        nd.delete(:parent_id)
+        nd.delete(:layer_id)
+        nd.delete(:created_at)
+        nd.delete(:updated_at)
+        nd.delete(:node_data_type)
+        nd.delete(:created_at)
+        nd.delete(:updated_at)
+
+        if Layer.isWebservice?(layer_id) and !params.has_key?('skip_webservice')
+          nd[:data] = WebService.load(layer_id, cdk_id, nd[:data])
+        end
+
+        nd[:data] = nest(nd[:data].to_hash)
+        newh[name] = nd
+      end
+      newh
+    end # def
   end # class
 end # module
 
