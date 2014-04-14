d747e7f9792bdbf44eed2c199e6fb8e6c8194372
Peter Sutton <foxxy@foxdogstudios.com>
2014-04-14 17:53:05 +0100

    Refactored edit layer route

--- cms/routes/layers/layer/post.rb
+++ cms/routes/layers/layer/post.rb
@@ -2,79 +2,172 @@
 
 module CitySDK
   class CMSApplication < Sinatra::Application
-    post '/layers/:layer_name/' do |layer_name|
+    post '/layers/:layer_name/?' do |layer_name|
       layer = Layer.for_name(layer_name)
-
       if layer.nil?
         halt 404, "No layer named #{ layer_name.inspect } exists."
       end # end
-
-      user = current_user
-
-      unless user.update_layer?(layer)
+      unless current_user.update_layer?(layer)
         halt 401, 'Not authorized.'
       end # if
 
-      # Description
-      layer.description = params.fetch('description')
-
-      # Realtime
-      layer.realtime = params.key?('realtime')
-
-      if layer.realtime
-        # Update rate
-        layer.update_rate = params.fetch('update_rate').to_i
-
-      else
-        # Validity
-        get_valid = lambda do |key|
-          valid = params.fetch("validity_#{ key }") do
-            Time.now.strftime('%Y-%m-%d')
-          end # do
-        end # do
-        valid_from = get_valid.call('from')
-        valid_to = get_valid.call('to')
-        layer.validity = "[#{ valid_from }, #{ valid_to }]"
-      end # else
-
-      # Data sources
-      data_sources = params.fetch('data_sources', []).to_a
-      data_sources.map! { |index, data_source|  [index.to_i, data_source] }
-      data_sources.sort!.map! { |_, data_source| data_source }
-
-      # XXX: What is this?
-      data_sources_x = params['data_sources_x']
-      data_sources << data_sources_x unless data_sources_x.nil?
-
-      data_sources.reject! { |data_source| data_source.empty? }
-      layer.data_sources = data_sources
-
-      # Organization
-      layer.organization = params.fetch('organization')
-
-      # Category
-      layer.category = params.fetch('category')
-
-      # Other attributes
-      layer.webservice = params['wsurl']
-
-      # Update rate
-      layer.update_rate = params['update_rate']
-
-      # Sample URL
-      sample_url = params['sample_url']
-      unless sample_url.nil? || sample_url.empty?
-        layer.sample_url = sample_url
-      end # if
+      LayerPostHandler.new(layer, params).handle()
 
       # Save or report errors.
-      if layer.valid?
-        layer.save
+      if layer.valid?()
+        layer.save()
         redirect '/layers/'
       else
         haml :edit_layer, locals: { layer: layer }
       end # else
     end # do
+  end # class
+
+  class LayerPostHandler
+    def initialize(layer, params)
+      @layer = layer
+      @params = params
+    end # def
+
+    def handle()
+      update_category()
+      update_data_sources()
+      update_description()
+      update_organization()
+      update_realtime()
+      update_sample_url()
+      update_update_rate()
+      update_validity()
+      update_webservice()
+    end # def
+
+    private
+
+    def validity?()
+      !(get_validity_from().nil? || get_validity_to().nil?)
+    end # def
+
+
+    # ======================================================================
+    # = Getters                                                            =
+    # ======================================================================
+
+    def get_category()
+      get('category')
+    end # def
+
+    def get_data_sources()
+      data_sources = get_or('data_sources', []).to_a()
+      data_sources.map! { |index, data_source|  [index.to_i, data_source] }
+      data_sources.sort!().map! { |_, data_source| data_source }
+      # XXX: What is this?
+      data_sources_x = try_get('data_sources_x')
+      data_sources << data_sources_x unless data_sources_x.nil?
+      data_sources.reject! { |data_source| data_source.empty? }
+    end # def
+
+    def get_description()
+      get('description')
+    end # def
+
+    def get_organization()
+      get('organization')
+    end # def
+
+    def get_realtime()
+      key?('realtime')
+    end # def
+
+    def get_sample_url()
+      sample_url = try_get('sample_url')
+      return if sample_url.nil? || sample_url.empty?
+      sample_url
+    end # def
+
+    def get_update_rate()
+      update_rate = try_get('update_rate')
+      update_rate = update_rate.to_i() unless update_rate.nil?
+      update_rate
+    end # def
+
+    def get_validity()
+      return unless validity?()
+      "[#{ get_validity_from() }, #{ get_validity_to() }]"
+    end # def
+
+    def get_validity_from()
+      try_get('validity_from')
+    end # def
+
+    def get_validity_to()
+      try_get('validity_to')
+    end # def
+
+    def get_webservice()
+      try_get('wsurl')
+    end # def
+
+
+    # ======================================================================
+    # = Updaters                                                           =
+    # ======================================================================
+
+    def update_category()
+      @layer.category = get_category()
+    end # def
+
+    def update_data_sources()
+      @layer.data_sources = get_data_sources()
+    end # def
+
+    def update_description()
+      @layer.description = get_description()
+    end # def
+
+    def update_organization()
+      @layer.organization = get_organization()
+    end # def
+
+    def update_realtime()
+      @layer.realtime = get_realtime()
+    end # def
+
+    def update_sample_url()
+      @layer.sample_url = get_sample_url()
+    end # def
+
+    def update_update_rate()
+      @layer.update_rate = get_update_rate()
+    end # def
+
+    def update_validity()
+      @layer.validity = get_validity()
+    end # def
+
+    def update_webservice()
+      @layer.webservice = get_webservice()
+    end # def
+
+
+    # ======================================================================
+    # = Helpers                                                            =
+    # ======================================================================
+
+    def key?(key)
+      @params.key?(key)
+    end # def
+
+    def get(key)
+      @params.fetch(key)
+    end # def
+
+    def try_get(key)
+      @params[key]
+    end # def
+
+    def get_or(key, default)
+      @params.fetch(key, default)
+    end # def
   end # class
 end # module
 
