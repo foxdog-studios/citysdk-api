13291214f73204e01ed5e605ce79a95b8e206eba
Peter Sutton <foxxy@foxdogstudios.com>
2014-04-15 08:46:30 +0100

    Removed unused route

--- server/routes/layers/reload__/get.rb
+++
@@ -1,8 +0,0 @@
-class CitySDKAPI < Sinatra::Application
-  get '/layers/reload__' do
-    @do_cache = false
-    CitySDKAPI::Layer.getLayerHashes
-    { status:  'success' }.to_json
-  end # do
-end # class
-
