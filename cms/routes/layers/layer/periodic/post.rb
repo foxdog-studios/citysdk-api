0524a5f1525551af02e1a8380a95753f92324a91
Peter Sutton <foxxy@foxdogstudios.com>
2014-04-16 10:10:58 +0100

    Save NULL if import_url is the empty string

--- cms/routes/layers/layer/periodic/post.rb
+++ cms/routes/layers/layer/periodic/post.rb
@@ -4,11 +4,15 @@ module CitySDK
   class CMSApplication < Sinatra::Application
     post '/layers/:layer_name/periodic' do |layer_name|
       layer = Layer.where(name: layer_name).first()
-      halt 404 if layer.nil?
+      halt 404 unless layer
       halt 401 unless current_user.update_layer?(layer)
 
+      # Import URL
       import_url = params['import_url']
-      layer.import_url = import_url unless import_url.nil?
+      if import_url
+        import_url = import_url.strip
+        layer.import_url = import_url.empty? ? nil : import_url
+      end # end
 
       if layer.valid?()
         layer.save()
